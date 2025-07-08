10fe8b2624f04afd7e67627b5ba62a21c38b2426
kishore-loganathan <kishoreloganathan773@gmail.com>
2025-07-08 10:24:11 +0530

    5th

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,4 +1,4 @@
-const Product = require("../models/Product");
+const Product = require("../models/productModels");
 
 const createProduct = async (req, res) => {
   try {
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,15 +2,14 @@ const express = require("express");
 const router = express.Router();
 const {
   createProduct,
-  getProducts,
-  getProductById,
+  getAllProducts,
 } = require("../controllers/productController");
 
 // optionally use auth middleware for admin-only creation
 const protect = require("../middleware/authMiddleware");
 
 router.post("/", protect, createProduct); // create product
-router.get("/", getProducts); // get all
-router.get("/:id", getProductById); // get one
+router.get("/", getAllProducts); // get all
+// router.get("/:id", getProductById); // get one
 
 module.exports = router;
